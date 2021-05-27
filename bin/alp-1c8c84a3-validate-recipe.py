32ef0ef8a6df4ef9aa6b218a52ad85ee759cddad
Liz Gehret <elizabeth.gehret@gmail.com>
2021-05-27 09:15:39 -0700

    more validation updates

--- bin/alp-1c8c84a3-validate-recipe.py
+++ bin/alp-1c8c84a3-validate-recipe.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 # TODO: validate that file exists
-# TODO: Validate that filepath exists (?) -no b/c if filepath exists
-# then it's assumed that the filepath exists as well...
+# TODO: Validate that filepath exists
 # TODO: validate that file contains version & name
 
 # import os.path
 import sys
 import pathlib
+import yaml
 
 if __name__ == '__main__':
     filename = sys.argv[0]
@@ -27,3 +27,19 @@ if __name__ == '__main__':
         print(f'This is the location of the file: ${filepath}')
     else:
         raise ValueError
+
+    # YAML file parsing
+    recipe = open('../tests/validate-recipe/data/recipe.yml')
+    parsed_recipe = yaml.load(recipe, Loader=yaml.FullLoader)
+
+    # Does the file contain a version
+    if parsed_recipe.get('version') == None:
+        raise ValueError
+    else:
+        print(parsed_recipe.get('version'))
+    
+    # Does the file contain a name
+    if parsed_recipe.get('name') == None:
+        raise ValueError
+    else:
+        print(parsed_recipe.get('name'))

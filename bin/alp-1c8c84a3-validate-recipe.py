3995b5992568e30dc31dc3b69f763f660cd7665e
Liz Gehret <elizabeth.gehret@gmail.com>
2021-05-28 07:57:08 -0700

    more testing

--- bin/alp-1c8c84a3-validate-recipe.py
+++ bin/alp-1c8c84a3-validate-recipe.py
@@ -16,6 +16,12 @@ if __name__ == '__main__':
     with open(filepath) as recipe:
         parsed_recipe = yaml.load(recipe, Loader=yaml.FullLoader)
 
+    # Does the file contain any disallowed keys
+    allowed_keys = ['version', 'name', 'type', 'build_cmd', 'requirements', 'test']
+    for parsed_recipe in parsed_recipe.keys():
+        if parsed_recipe not in allowed_keys:
+            raise ValueError(f'invalid key added. allowed keys are: {allowed_keys}')
+
     # Does the file contain the version key and a value
     if parsed_recipe['version'] == None:
         raise ValueError('version value not added')
@@ -24,6 +30,7 @@ if __name__ == '__main__':
     if parsed_recipe['name'] == None:
         raise ValueError('name value not added')
 
-    # # Does the file contain the type key and a valid value
-    if parsed_recipe['type'] not in ['plugin', 'interface', 'utility']:
-        raise ValueError('valid type value not added. allowed values are: plugin, interface, or utility')
+    # Does the file contain the type key and a valid value
+    allowed_types = ['plugin', 'interface', 'utility']
+    if parsed_recipe['type'] not in allowed_types:
+        raise ValueError(f'valid type value not added. allowed values are: {allowed_types}')
--- tests/validate-recipe/test.py
+++ tests/validate-recipe/test.py
@@ -4,47 +4,69 @@ import unittest
 import subprocess
 
 def run(filepath):
-    results = subprocess.run(['python', '../../bin/alp-1c8c84a3-validate-recipe.py', filepath])
-    return results.returncode
+    results = subprocess.run(['python', '../../bin/alp-1c8c84a3-validate-recipe.py', filepath], capture_output=True)
+    return [results.returncode, results.stderr]
 
 class TestValidateRecipe(unittest.TestCase):
 
     def test_basic(self):
         filepath = '../../tests/validate-recipe/data/recipe-basic.yml'
-        obs_exit_code = run(filepath)
+        obs_exit_code, obs_stderr = run(filepath)
         self.assertEqual(obs_exit_code, 0)
+        self.assertEqual(obs_stderr, b'')
 
     def test_version_not_provided(self):
         # No version key present
-        obs_exit_code = run('../../tests/validate-recipe/data/recipe-noversion-key.yml')
-        self.assertNotEqual(obs_exit_code, 0)        
+        filepath = '../../tests/validate-recipe/data/recipe-noversion-key.yml'
+        obs_exit_code, obs_stderr = run(filepath)
+        self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'KeyError' in obs_stderr)
 
         # Version key present, no value
-        obs_exit_code = run('../../tests/validate-recipe/data/recipe-noversion-value.yml')
+        filepath = '../../tests/validate-recipe/data/recipe-noversion-value.yml'
+        obs_exit_code, obs_stderr = run(filepath)
         self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'version value not added' in obs_stderr)
 
     def test_name_not_provided(self):
         # No name key present
-        obs_exit_code = run('../../tests/validate-recipe/data/recipe-noname-key.yml')
+        filepath = '../../tests/validate-recipe/data/recipe-noname-key.yml'
+        obs_exit_code, obs_stderr = run(filepath)
         self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'KeyError' in obs_stderr)
 
         # Name key present, no value
-        obs_exit_code = run('../../tests/validate-recipe/data/recipe-noname-value.yml')
+        filepath = '../../tests/validate-recipe/data/recipe-noname-value.yml'
+        obs_exit_code, obs_stderr = run(filepath)
         self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'name value not added' in obs_stderr)
 
     def test_type_not_valid(self):
         # No type key present
-        obs_exit_code = run('../../tests/validate-recipe/data/recipe-notype-key.yml')
+        filepath = '../../tests/validate-recipe/data/recipe-notype-key.yml'
+        obs_exit_code, obs_stderr = run(filepath)
         self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'KeyError' in obs_stderr)
 
         # Invalid type value present
-        obs_exit_code = run('../../tests/validate-recipe/data/recipe-invalidtype-value.yml')
+        filepath = '../../tests/validate-recipe/data/recipe-invalidtype-value.yml'
+        obs_exit_code, obs_stderr = run(filepath)
         self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'valid type value not added' in obs_stderr)
+
+    def test_key_not_valid(self):
+        filepath = '../../tests/validate-recipe/data/recipe-invalid-key.yml'
+        obs_exit_code, obs_stderr = run(filepath)
+        self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'invalid key added' in obs_stderr)
 
     def test_file_does_not_exist(self):
-        obs_exit_code = run('../../tests/validate-recipe/data/norecipe.yml')
+        filepath = '../../tests/validate-recipe/data/norecipe.yml'
+        obs_exit_code, obs_stderr = run(filepath)
         self.assertNotEqual(obs_exit_code, 0)
+        self.assertTrue(b'filepath not found' in obs_stderr)
 
+    #TODO: fix this unit test
     def test_filepath_not_provided(self):
         results = subprocess.run(['python', '../../bin/alp-1c8c84a3-validate-recipe.py'])
         obs_exit_code = results.returncode
